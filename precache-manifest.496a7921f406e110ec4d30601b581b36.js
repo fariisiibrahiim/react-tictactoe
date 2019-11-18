449c7a83339d585fa4eb1e806b0e78a1be31a6ee
fariisiibrahiim <farisibrahmi@gmail.com>
2019-11-18 10:32:43 +0700

    Updates

---
+++ precache-manifest.496a7921f406e110ec4d30601b581b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb21fe91c1317475558b5b72e376061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6c12307ee5312005e1",
+    "url": "/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "db08d5e8eae505b50534",
+    "url": "/static/js/2.f34c9ee1.chunk.js"
+  },
+  {
+    "revision": "ac6c12307ee5312005e1",
+    "url": "/static/js/main.03c6ed33.chunk.js"
+  },
+  {
+    "revision": "b63362eb8be8605a4c5d",
+    "url": "/static/js/runtime-main.37e58f78.js"
+  }
+]);
